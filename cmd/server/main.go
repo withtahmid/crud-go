b37b092d72acf4dda283215702caf8077f209bc3
withtahmid <withtahmid@gmai.com>
2025-12-07 01:01:38 +0600

    CRU-d

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,14 +6,22 @@ import (
 
 	"github.com/withtahmid/crud-go/internal/database"
 	"github.com/withtahmid/crud-go/internal/handlers"
+	"github.com/withtahmid/crud-go/internal/middleware"
 )
 
 func main(){
 	database.Connect()
 	
-	http.HandleFunc("/list", handlers.GetList)
+	mux := http.NewServeMux();
+
+
+	mux.HandleFunc("/todos", handlers.GetList)
+	mux.HandleFunc("/todos/update", handlers.UpdateTodo)
+	mux.HandleFunc("/todos/create", handlers.CreateTodo)
+	mux.HandleFunc("/todos/", handlers.GetById)
 	
-	
+	handler := middleware.CORS(mux) 
+
 	log.Println("Server running on :8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", handler)
 }
--- internal/handlers/todo.go
+++ internal/handlers/todo.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/withtahmid/crud-go/internal/database"
 )
@@ -32,4 +33,110 @@ func GetList(w http.ResponseWriter, r *http.Request){
 	}
 	w.Header().Set("Content-Type", "Application/json")
 	json.NewEncoder(w).Encode(todos)
+}
+
+func GetById(w http.ResponseWriter, r *http.Request){
+
+
+
+	// id := r.URL.Query().Get("id")
+
+	// if id == ""{
+	// 	http.Error(w, "Missing id parameter", http.StatusBadRequest)
+	// 	return
+	// }
+
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 3 || parts[2] == ""{
+		http.Error(w, "Missing ID", http.StatusBadRequest)
+		return
+	}
+	id := parts[2]
+
+	query := "SELECT id, name, complete from todos where id = $1"
+	
+	var todo ToDo
+
+	err := database.DB.QueryRow(r.Context(), query, id).Scan(
+		&todo.ID, &todo.Name, &todo.Complete,
+	)
+
+	if err != nil {
+		http.Error(w, "Todo Not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Application-Type", "Application.json")
+	json.NewEncoder(w).Encode(todo)
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request){
+	if r.Method  != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var input struct { 
+		Name string `json:"name"`
+	}
+
+	err:= json.NewDecoder(r.Body).Decode(&input)
+	if err != nil{
+		http.Error(w, "Invalid Input", http.StatusBadRequest)
+		return
+	}
+
+	query := "INSERT INTO todos(name, complete) VALUES($1, $2) RETURNING id, name, complete"
+
+	var todo ToDo
+	err = database.DB.QueryRow(r.Context(), query, input.Name, false).Scan(
+		&todo.ID, &todo.Name, &todo.Complete,
+	)
+
+	if err != nil {
+		http.Error(w, "Failed to insert", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(todo)
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request){
+
+	if r.Method != http.MethodPut {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
+
+	var input ToDo
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	query := "UPDATE todos set name = $2, complete = $3 where id = $1 RETURNING id, name, complete"
+	var todo ToDo
+	err = database.DB.QueryRow(r.Context(), query, input.ID, input.Name, input.Complete).Scan(
+		&todo.ID, &todo.Name, &todo.Complete,
+	)
+	if err != nil {
+		http.Error(w, "Failed to update todo", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(todo)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodDelete{
+		http.Error(w, "Invalid Method", http.StatusBadRequest)
+		return
+	}
+
+	
+
 }
